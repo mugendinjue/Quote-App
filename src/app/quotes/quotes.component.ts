b49593f571f0f200f914151c70bef20f289265b9
mugendinjue <mugendinjue7@gmail.com>
2019-08-16 13:40:53 +0300

    Add quoteDetails component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,8 +9,8 @@ import { Quotes } from '../quotes'
 export class QuotesComponent implements OnInit {
 
   quotesArr : Quotes [] = [
-    new Quotes (1,'denis','The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.',new Date(2019,5,10),10,15),
-    new Quotes (2,'denis','The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.',new Date(2019,5,10),15,30)
+    new Quotes (1,'denis','The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.',new Date(2019,5,10),0,0),
+    new Quotes (2,'denis','The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.',new Date(2019,5,10),0,0)
   ]
 
   constructor() { }
