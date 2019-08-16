105142ab197ef22d1d943a8937994f03d0acf89a
mugendinjue <mugendinjue7@gmail.com>
2019-08-16 17:26:50 +0300

    Add highlight directive

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,7 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
+  transform(value: any): number {
+
+      let today:Date = new Date(); 
+      let todayWithoutTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+      var dateDifference = Math.abs(value - todayWithoutTime) 
+      const secondsInDay = 86400; 
+      var dateDifferenceSeconds = dateDifference*0.001;
+      var dateCounter = dateDifferenceSeconds/secondsInDay;
+
+      console.log (value);
+
+      if (dateCounter >= 1 && value > todayWithoutTime){
+        return dateCounter;
+      }else{
+        return 0;
+      }
+
     return null;
   }
 
