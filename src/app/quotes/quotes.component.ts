086382d4975fa2bff4bf2ae5200cb8c535b6c3f6
mugendinjue <mugendinjue7@gmail.com>
2019-08-16 09:29:15 +0300

    Add a bootstrap cdn link to index.html

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes'
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  quotesArr : Quotes [] = [
+    new Quotes (1,'denis','The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.',new Date(2019,5,10),10,15),
+    new Quotes (2,'denis','The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.',new Date(2019,5,10),15,30)
+  ]
 
   constructor() { }
 
