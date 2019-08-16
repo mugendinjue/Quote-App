f81c7ff984211739f06dfa6ddeffd34610a3545f
mugendinjue <mugendinjue7@gmail.com>
2019-08-16 09:13:43 +0300

    Add a component quotes

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,5 @@
 export class Quotes {
+    constructor( public id : number, public author : string,public quote : string , public postDate : Date , public like : number , public disLike : number){
+
+    }
 }
