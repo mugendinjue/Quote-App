ff592601e53d84119a561dcbd697abdcddcf9f65
mugendinjue <mugendinjue7@gmail.com>
2019-08-17 15:52:31 +0300

    Add vote count

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -10,9 +10,17 @@ export class QuoteDetailsComponent implements OnInit {
 
   @Input () showDetails : Quotes; 
   @Output () willDelete = new EventEmitter<boolean>();
+  @Output () willAdd = new EventEmitter<boolean>();
+  @Output () willDlike = new EventEmitter<boolean>();
   
 
-  
+  addLike (add : boolean){
+    this.willAdd.emit(add);
+  }
+
+  addDisLike (add : boolean){
+    this.willDlike.emit(add);
+  }
 
   deleteDiary(done : boolean){
     this.willDelete.emit(done);
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,21 +9,37 @@ import { Quotes } from '../quotes'
 export class QuotesComponent implements OnInit {
 
   quotesArr : Quotes [] = [
-    new Quotes (1,' Winston Churchill','Denis','The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.',new Date(2019,5,10),0,0),
-    new Quotes (2,' Walt Disney','Christine','The Way Get Started Is To Quit Talking And Begin Doing.',new Date(2019,5,15),0,0),
-    new Quotes (3,' Theodore Roosevelt','Malcom','Do What You Can With All You Have, Wherever You Are.',new Date(2019,5,20),0,0),
-    new Quotes (4,' Pablo Picasso','Washu Code','He can who thinks he can, and he cant who thinks he cant. This is an inexorable, indisputable law.',new Date(2019,5,15),0,0),
-    new Quotes (5,' Albert Einstein','Hillary','Creativity Is Intelligence Having Fun. ',new Date(2019,5,25),0,0),
-    new Quotes (6,' Steve Jobs','Mercy','If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.',new Date(2019,5,30),0,0),
-    new Quotes (7,' Brian Tracy','Brian','Develop An ‘Attitude Of Gratitude’. Say Thank You To Everyone You Meet For Everything They Do For You.',new Date(2019,6,1),0,0),
-    new Quotes (8,' Steve Jobs','Jayrene','The Only Way To Do Great Work Is To Love What You Do. If You Haven’t Found It Yet, Keep Looking. Don’t Settle.',new Date(2019,6,10),0,0),
-    new Quotes (9,' Walt Disney','Genson','All our dreams can come true, if we have the courage to pursue them.',new Date(2019,6,15),0,0),
-    new Quotes (10,' Aristotle','Steve','We are what we repeatedly do. Excellence, then, is not an act, but a habit.',new Date(2019,6,19),0,0),
+    new Quotes (1,' Winston Churchill','Denis','The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.',new Date(2019,5,10),20,2),
+    new Quotes (2,' Walt Disney','Christine','The Way Get Started Is To Quit Talking And Begin Doing.',new Date(2019,5,15),35,5),
+    new Quotes (3,' Theodore Roosevelt','Malcom','Do What You Can With All You Have, Wherever You Are.',new Date(2019,5,20),19,3),
+    new Quotes (4,' Pablo Picasso','Washu Code','He can who thinks he can, and he cant who thinks he cant. This is an inexorable, indisputable law.',new Date(2019,5,15),15,2),
+    new Quotes (5,' Albert Einstein','Hillary','Creativity Is Intelligence Having Fun. ',new Date(2019,5,25),17,3),
+    new Quotes (6,' Steve Jobs','Mercy','If You Are Working On Something That You Really Care About, You Don’t Have To Be Pushed. The Vision Pulls You.',new Date(2019,5,30),21,3),
+    new Quotes (7,' Brian Tracy','Brian','Develop An ‘Attitude Of Gratitude’. Say Thank You To Everyone You Meet For Everything They Do For You.',new Date(2019,6,1),23,6),
+    new Quotes (8,' Steve Jobs','Jayrene','The Only Way To Do Great Work Is To Love What You Do. If You Haven’t Found It Yet, Keep Looking. Don’t Settle.',new Date(2019,6,10),31,8),
+    new Quotes (9,' Walt Disney','Genson','All our dreams can come true, if we have the courage to pursue them.',new Date(2019,6,15),27,10),
+    new Quotes (10,' Aristotle','Steve','We are what we repeatedly do. Excellence, then, is not an act, but a habit.',new Date(2019,6,19),13,5),
 
   ]
 
   move(index){
     this.quotesArr[index].show = !this.quotesArr[index].show;
+  }
+
+  likeAdd(willAdd,index){
+    if( willAdd ){
+      let likeNum = this.quotesArr[index].like;
+      this.quotesArr[index].like = likeNum + 1;
+      
+    }
+  }
+
+  likeDisAdd(willDlike,index){
+    if( willDlike ){
+      let disLikeNum = this.quotesArr[index].disLike;
+      this.quotesArr[index].disLike = disLikeNum + 1;
+      
+    }
   }
 
   removeDiary(willDelete,index){
