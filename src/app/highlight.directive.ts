ec7431c24bf5f244e18ea6968d6d30804604b17d
mugendinjue <mugendinjue7@gmail.com>
2019-08-19 07:52:02 +0300

    add a display section

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -8,16 +8,16 @@ export class HighlightDirective {
 
   numberOfClicks = 0;
 
-  @HostListener ('click') onclick(){
-    this.quoteColor(" #764ba2")
-  }
+  // @HostListener ('click') onclick(){
+  //   this.quoteColor(" #764ba2")
+  // }
 
 
 
-  constructor(private elem:ElementRef) { }
+  constructor(private elem:ElementRef) {   this.elem.nativeElement.style.color="#764ba2"; }
 
-  private quoteColor(action:string){
-    this.elem.nativeElement.style.color=action;
-  }
+  // private quoteColor(action:string){
+  //   this.elem.nativeElement.style.color=action;
+  // }
 
 }
